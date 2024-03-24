a27fb4b24e772d5db365d9b4ca2af250286d10b6
Sviatoslav <sviatoslav.tkachuk11@gmail.com>
2024-03-24 13:42:20 +0200

    Lab 4

--- edge/app/usecases/data_processing.py
+++ edge/app/usecases/data_processing.py
@@ -12,4 +12,8 @@ def process_agent_data(
     Returns:
         processed_data_batch (ProcessedAgentData): Processed data containing the classified state of the road surface and agent data.
     """
-    # Implement it
+    y_value = agent_data.accelerometer.y
+
+    road_state = "bit" if y_value < 0 else "normal"
+
+    return ProcessedAgentData(road_state=road_state, agent_data=agent_data)
